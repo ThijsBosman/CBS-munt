a46c7df7a4be2e7243e49d64f0e438b58f797567
Thijs Bosman <thijs.bosman2005@gmail.com>
2024-04-23 22:03:08 +0200

    Minor changes

---
+++ src/binomial.py
@@ -0,0 +1,73 @@
+import numpy as np
+from scipy.stats import binom
+from scipy.stats._distn_infrastructure import rv_frozen
+
+
+def get_binom_distribution(n: int = 100, p: float = 0.5) -> rv_frozen:
+    """This function returns the binomial distribution of a given n and p
+
+    Args:
+        n (int): number of trials
+        p (float): probability of success
+
+    Returns:
+        rv_frozen: binomial distribution
+    """
+    if n < 0:
+        raise ValueError('The number of trials must be non-negative.')
+
+    if p < 0 or p > 1:
+        raise ValueError('The probability of success must be between 0 and 1.')
+
+    return binom(n, p)
+
+
+def get_binom_pdf(distribution: rv_frozen) -> np.ndarray:
+    """This function returns the probability density function of a given binomial distribution
+
+    Args:
+        distribution (rv_frozen): binomial distribution
+
+    Returns:
+        numpy.ndarray: probability density function
+    """
+    if distribution is None:
+        raise ValueError('The distribution must be provided.')
+
+    return distribution.pmf(np.arange(distribution.args[0] + 1))
+
+
+def get_binom_cdf(distribution: rv_frozen = None) -> np.ndarray:
+    """This function returns the cumulative density function of a given binomial distribution
+
+    Args:
+        distribution (rv_frozen): binomial distribution
+
+    Returns:
+        numpy.ndarray: cumulative density function
+    """
+    if distribution is None:
+        raise ValueError('The distribution must be provided.')
+
+    return distribution.cdf(np.arange(distribution.args[0] + 1))
+
+
+def get_rejecting_boundries(distribution: rv_frozen = None, alpha: float = 0.05) -> tuple:
+    """This function returns the rejecting boundries of a given binomial distribution.
+    Values lower than the first boundry and higher than the second boundry are rejected.
+    Note that the boundries are not inclusive.
+
+    Args:
+        distribution (rv_frozen): binomial distribution
+        alpha (float): significance level
+
+    Returns:
+        tuple: rejecting boundries
+    """
+    if distribution is None:
+        raise ValueError('The distribution must be provided.')
+
+    if alpha < 0 or alpha > 1:
+        raise ValueError('The significance level must be between 0 and 1.')
+
+    return distribution.ppf(alpha / 2), distribution.ppf(1 - alpha / 2)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,30 @@
+import binomial
+import matplotlib.pyplot as plt
+
+def main() -> None:
+    """Main function of the program.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    x = binomial.get_binom_distrobution(1000, 0.5)
+    
+    # Plot the probability density function
+    plt.plot(binomial.get_binom_pdf(x))
+    plt.title('Probability Density Function')
+    plt.xlabel('Number of Successes')
+    plt.ylabel('Probability')
+    plt.show()
 
 
-def main():
-    print("Hello World!")
+
+
+
+
+
 
 if __name__ == '__main__':
     main()
